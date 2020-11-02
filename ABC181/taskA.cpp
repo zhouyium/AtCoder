f2a2cf2feda2a4213178bcf85fc8c21ca9c8e954
周毅 <yc07444@um.edu.mo>
2020-11-02 21:31:01 +0800

    增加 ABC181 A题。

---
+++ ABC181/taskA.cpp
@@ -0,0 +1,14 @@
+    //https://atcoder.jp/contests/abc181/tasks/abc181_a
+    //A - Heavy Rotation
+    #include <iostream>
+    using namespace std;
+    int main() {
+        int n;
+        cin>>n;
+        if (0==n%2) {
+            cout<<"White\n";
+        } else {
+            cout<<"Black\n";
+        }
+        return 0;
+    }
